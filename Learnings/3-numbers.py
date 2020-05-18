6c5d497f0ef5ad2294f5e20a4d7639356773f3f7
Nickinfinity <nikhilg.me@gmail.com>
2020-05-18 22:36:11 +0530

    Handling Numerals

---
+++ Learnings/3-numbers.py
@@ -0,0 +1,25 @@
+# NIKHIL GUPTA
+# 18 May 2020
+# Playing with numbers
+#
+import this #Zen of Python
+print(4*2)
+print(4+2)
+print(4-2)
+print(4/2)  # Division return float
+print(4//2) # but this returns integer of quotient
+print(4%2)
+print(4**2)
+print(14_00_12) # for python 3.6 or later
+#
+# MULTIPLE ASSIGNMENT
+x,y,z = 1,3,5
+print(x*y*z)
+#
+# No constants in python just use upper case and dont change
+MAX_CONNECTIONS = 50
+print(MAX_CONNECTIONS)
+#
+number = 16
+print(f"My favourite number is: {number}")
+print("My favourite number is: {}".format(number))

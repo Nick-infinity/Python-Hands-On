753a808e5f8bcf38ceacc7d80254f2546d4a6fea
Nickinfinity <nikhilg.me@gmail.com>
2020-05-18 22:38:53 +0530

    Learning Tuples

---
+++ Learnings/6-tuples.py
@@ -0,0 +1,16 @@
+# Nikhil Gupta
+# 18 May 2020
+# This program intriduces us to "Tuples"
+#
+weapon_damage = (10,20,30)
+print(weapon_damage[0])
+# weapon_damage[0] = 60
+# TypeError: 'tuple' object does not support item assignment
+#
+single_element_tuple = ("myLife",)
+print(single_element_tuple[0])
+#
+# Whole tuple can be re-written with same variable name
+#
+weapon_damage = [20,30,40,50]
+print(weapon_damage)
